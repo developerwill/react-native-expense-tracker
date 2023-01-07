927b8986261a0428b9abcfc162f110e3acbe818d
developerwill <info@wilsongomes.me>
2023-01-07 20:00:53 +0100

    Dummy data restructured

---
+++ data/DUMMY_DATA.js
@@ -0,0 +1,52 @@
+const DUMMY_EXPENSES = [
+    {
+        id: 'e1',
+        description: 'A pair of shoes',
+        amount: 59.99,
+        date: new Date('2021-12-19')
+    },
+    {
+        id: 'e2',
+        description: 'A pair of trousers',
+        amount: 89.99,
+        date: new Date('2023-01-01')
+    },
+    {
+        id: 'e3',
+        description: 'Bananas',
+        amount: 5.99,
+        date: new Date('2020-02-15')
+    }/*,
+    {
+        id: 'e4',
+        description: 'Book',
+        amount: 14.54,
+        date: new Date('2023-12-07')
+    },
+    {
+        id: 'e5',
+        description: 'A pair of shoes',
+        amount: 59.99,
+        date: new Date('2021-12-19')
+    },
+    {
+        id: 'e6',
+        description: 'A pair of trousers',
+        amount: 89.99,
+        date: new Date('2022-01-05')
+    },
+    {
+        id: 'e7',
+        description: 'Bananas',
+        amount: 5.99,
+        date: new Date('2020-02-15')
+    },
+    {
+        id: 'e8',
+        description: 'Book',
+        amount: 14.54,
+        date: new Date('2023-12-07')
+    }*/
+];
+
+export default DUMMY_EXPENSES;
