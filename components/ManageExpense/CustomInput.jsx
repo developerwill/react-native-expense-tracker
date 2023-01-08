500692ee159f48bc2539e62f6e0258a9ad4f7156
developerwill <info@wilsongomes.me>
2023-01-08 13:22:14 +0100

    Form Layout

--- components/ManageExpense/CustomInput.jsx
+++ components/ManageExpense/CustomInput.jsx
@@ -1,7 +1,7 @@
 import { TextInput, View, Text, StyleSheet } from 'react-native';
 import { GlobalStyles } from '../../constants/styles';
 
-export default function CustomInput({label, textInputConfig}) {
+export default function CustomInput({label, textInputConfig, style}) {
     const inputStyles = [styles.input];
 
     if(textInputConfig?.multiline) {
@@ -9,7 +9,7 @@ export default function CustomInput({label, textInputConfig}) {
     }
 
     return (
-        <View style={styles.container}>
+        <View style={[styles.container, style]}>
             <Text style={styles.label}>{label}</Text>
             <TextInput style={inputStyles} {...textInputConfig}/>
         </View>
--- screens/ExpenseForm.jsx
+++ screens/ExpenseForm.jsx
@@ -1,22 +1,25 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import CustomInput from '../components/ManageExpense/CustomInput';
 
 export default function ExpenseForm() {
     function amountChangeHandler(){}
 
     return (
-        <View>
-            <CustomInput label={'Amount'} textInputConfig={{
-                keyboardType: 'decimal-pad',
-                onChangeText: amountChangeHandler
-            }}
-            />
-            <CustomInput label={'Date'} textInputConfig={{
-                placeholder: 'YYYY-MM-DD',
-                maxLength: 10,
-                onChangeText: () => {}
-            }}
-            />
+        <View style={styles.container}>
+            <Text style={styles.title}>Your Expense</Text>
+            <View style={styles.amountDate}>
+                <CustomInput label={'Amount'} style={styles.input} textInputConfig={{
+                    keyboardType: 'decimal-pad',
+                    onChangeText: amountChangeHandler
+                }}
+                />
+                <CustomInput label={'Date'} style={styles.input} textInputConfig={{
+                    placeholder: 'YYYY-MM-DD',
+                    maxLength: 10,
+                    onChangeText: () => {}
+                }}
+                />
+            </View>
             <CustomInput label={'Description'} textInputConfig={{
                 multiline: true,
                 numberOfLines: 4
@@ -26,4 +29,21 @@ export default function ExpenseForm() {
     );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+      marginTop: 40
+    },
+    amountDate: {
+        flexDirection: 'row',
+    },
+    input: {
+        flex: 1
+    },
+    title: {
+        fontSize: 24,
+        color: 'white',
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginBottom: 40
+    }
+});
